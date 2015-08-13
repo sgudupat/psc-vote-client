361104289d26d6293c98d0e8b5f4e5942cddd1cd
sgudupat <sandeep.g@pscglobalsolutions.com>
2015-08-13 13:15:04 +0530

    Fixed layouts for Sign In and Register

--- src/com/client/vote/HomePageActivity.java
+++ src/com/client/vote/HomePageActivity.java
@@ -10,6 +10,6 @@ public class HomePageActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.client_signin);
     }
 }
