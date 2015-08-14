92e3fdd32eee5c797cb5fc096eedd6116c48c4e4
murali333 <murali.n333@gmail.com>
2015-08-14 13:45:22 +0530

    campaign code

--- src/com/client/vote/CampaignActivity.java
+++ src/com/client/vote/CampaignActivity.java
@@ -1,14 +1,32 @@
 package com.client.vote;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class CampaignActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class CampaignActivity  extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
+	  //  super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.campaign);
-    }
-}
+	    //setContentView( R.layout.campaign );
+		 super.onCreate(savedInstanceState);
+
+		    setContentView(R.layout.campaign); 
+
+		    //generate list
+		    ArrayList<String> list = new ArrayList<String>();
+		    list.add("loreal");
+		    list.add("fiama");
+
+		    //instantiate custom adapter
+		    CampaignAdapter adapter = new CampaignAdapter(list, this);
+
+		    //handle listview and assign adapter
+		    ListView lView = (ListView)findViewById(R.id.list_view);
+		    lView.setAdapter(adapter);
+	}
+	}
