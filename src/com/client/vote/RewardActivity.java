58d60c8514c1a97f7a94b773ea29ed25cc1531e2
sgudupat <sandeep.g@pscglobalsolutions.com>
2015-08-28 11:31:50 +0530

    Removed the dependency of external jar for image upload

--- src/com/client/vote/RewardActivity.java
+++ src/com/client/vote/RewardActivity.java
@@ -1,11 +1,5 @@
 package com.client.vote;
 
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -21,25 +15,21 @@ import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
-
+import android.widget.*;
 import com.client.vote.common.SimpleHttpClient;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class RewardActivity extends Activity {
 
-	 ProgressDialog prgDialog;
-	    String encodedString;
-	    RequestParams params = new RequestParams();
-	    String imgPath, fileName;
-	    Bitmap bitmap;
-	    private static int RESULT_LOAD_IMG = 1;
+    ProgressDialog prgDialog;
+    String encodedString;
+    String imgPath, fileName;
+    Bitmap bitmap;
+    private static int RESULT_LOAD_IMG = 1;
     String anchorName;
 
     @Override
@@ -75,25 +65,25 @@ public class RewardActivity extends Activity {
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                 imgPath = cursor.getString(columnIndex);
                 cursor.close();
-               
+
                 ImageView imgView = (ImageView) findViewById(R.id.reward_imageView1);
                 // Set the Image in ImageView
                 imgView.setImageBitmap(BitmapFactory.decodeFile(imgPath));
-           
+
                 String fileNameSegments[] = imgPath.split("/");
                 fileName = fileNameSegments[fileNameSegments.length - 1];
-                Log.i("imageview filename ", fileName );
+                Log.i("image view filename ", fileName);
                 // Put file name in Async Http Post Param which will used in jsp web app
-                params.put("filename", fileName);
                 // Get the Image's file name
             } else {
                 Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
             }
         } catch (Exception e) {
-        	//Log.i("exception",e.getMessage());
+            //Log.i("exception",e.getMessage());
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
     // When Upload button is clicked
     public void uploadImages() {
         // When Image is selected from Gallery
@@ -101,118 +91,56 @@ public class RewardActivity extends Activity {
             prgDialog.setMessage("Converting Image to Binary Data");
             prgDialog.show();
             // Convert image to String using Base64
-            encodeImagetoString();
-        // When Image is not selected from Gallery
+            encodeImage();
+            // When Image is not selected from Gallery
         } else {
-            Toast.makeText(
-                    getApplicationContext(),
-                    "You must select image from gallery before you try to upload",
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "You must select image from gallery before you try to upload", Toast.LENGTH_LONG).show();
         }
     }
- 
+
     // AsyncTask - To convert Image to String
-    public void encodeImagetoString() {
+    public void encodeImage() {
         new AsyncTask<Void, Void, String>() {
- 
-            protected void onPreExecute() {
- 
-            };
- 
+
             @Override
             protected String doInBackground(Void... params) {
                 BitmapFactory.Options options = null;
                 options = new BitmapFactory.Options();
                 options.inSampleSize = 3;
-                bitmap = BitmapFactory.decodeFile(imgPath,
-                        options);
+                bitmap = BitmapFactory.decodeFile(imgPath, options);
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 // Must compress the Image to reduce image size to make upload easy
-                bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream); 
+                bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
                 byte[] byte_arr = stream.toByteArray();
                 // Encode Image to String
                 encodedString = Base64.encodeToString(byte_arr, 0);
                 return "";
             }
- 
+
             @Override
             protected void onPostExecute(String msg) {
                 prgDialog.setMessage("Calling Upload");
-                // Put converted Image string into Async Http Post param
-                params.put("image", encodedString);
-                // Trigger Image upload
-                triggerImageUpload();
+                uploadImage();
             }
         }.execute(null, null, null);
     }
-     
-    public void triggerImageUpload() {
-        makeHTTPCall();
-    }
- 
+
     // Make Http call to upload Image to Php server
-    public void makeHTTPCall() {
-      /*  prgDialog.setMessage("uploading image to server");        
-        AsyncHttpClient client = new AsyncHttpClient();
-        // Don't forget to change the IP address to your LAN address. Port no as well.
-        client.post("http://52.74.246.67:8080/upload.jsp",
-                params, new AsyncHttpResponseHandler() {
-                    // When the response returned by REST has Http
-                    // response code '200'
-                    @Override
-                    public void onSuccess(String response) {
-                        // Hide Progress Dialog
-                        prgDialog.hide();
-                        //Toast.makeText(getApplicationContext(), response,Toast.LENGTH_LONG).show();
-                    }
- 
-                    // When the response returned by REST has Http
-                    // response code other than '200' such as '404',
-                    // '500' or '403' etc
-                    @Override
-                    public void onFailure(int statusCode, Throwable error,
-                            String content) {
-                        // Hide Progress Dialog
-                        prgDialog.hide();
-                        // When Http response code is '404'
-                        if (statusCode == 404) {
-                            Toast.makeText(getApplicationContext(),
-                                    "Requested resource not found",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                        // When Http response code is '500'
-                        else if (statusCode == 500) {
-                            Toast.makeText(getApplicationContext(),
-                                    "Something went wrong at server end",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                        // When Http response code other than 404, 500
-                        else {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Error Occured \n Most Common Error: \n1. Device not connected to Internet\n2. Web App is not deployed in App server\n3. App server is not running\n HTTP Status code : "
-                                            + statusCode, Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    }
-                });*/
-    	
-    	 final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-         postParameters.add(new BasicNameValuePair("image", encodedString));
-         postParameters.add(new BasicNameValuePair( "filename", fileName));
-        
-         try {
-             String response = SimpleHttpClient.executeHttpPost("/uploadImage", postParameters);
-             Log.i("Response:", response);
-            
-         } catch (Exception e) {
-             Log.i("Response 2:Error:", e.getMessage());
-             Toast.makeText(getApplicationContext(), "Reward creation failed", Toast.LENGTH_LONG).show();
-         }
+    public void uploadImage() {
+        final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+        postParameters.add(new BasicNameValuePair("image", encodedString));
+        postParameters.add(new BasicNameValuePair("filename", fileName));
+        try {
+            String response = SimpleHttpClient.executeHttpPost("/uploadImage", postParameters);
+            Log.i("Response:", response);
+        } catch (Exception e) {
+            Log.i("Response 2:Error:", e.getMessage());
+            Toast.makeText(getApplicationContext(), "Reward creation failed", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void createReward(View view) {
-    	uploadImages();
+        uploadImages();
         final EditText pushValue = (EditText) findViewById(R.id.rwd_pushLimitValue);
         RadioGroup rgRegion = (RadioGroup) findViewById(R.id.rwd_pushRegionGrp);
         RadioButton rbRegion = (RadioButton) findViewById(rgRegion.getCheckedRadioButtonId());
